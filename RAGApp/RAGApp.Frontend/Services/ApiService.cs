7edd5680cd1fdb4f569c03933bf6c27c41b0d2f3
Prashant Mahato <prashantmahato15@gmail.com>
2024-11-04 16:32:12 +0200

    Add files via upload

---
+++ RAGApp/RAGApp.Frontend/Services/ApiService.cs
@@ -0,0 +1,28 @@
+﻿namespace RAGApp.Frontend.Services
+{
+	public class ApiService
+	{
+	}
+}
+
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+public class ApiService
+{
+	private readonly HttpClient _httpClient;
+
+	public ApiService(HttpClient httpClient)
+	{
+		_httpClient = httpClient;
+	}
+
+	public async Task<string> GetInsightsAsync(string profession, string query, float topP)
+	{
+		var response = await _httpClient.PostAsJsonAsync("api/query", new { profession, query, topP });
+		response.EnsureSuccessStatusCode();
+		return await response.Content.ReadAsStringAsync();
+	}
+}
